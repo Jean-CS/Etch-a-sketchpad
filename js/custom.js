cf09d2699ac08404e580f8da5a90c993f197f7f3
Jean C. Santos <jeansantosc@gmail.com>
2016-03-09 17:52:41 -0300

    probably finished

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,11 @@
-$(document).ready(function() {
-	createGrid();
+var INITIALSIZE = 16;
 
+$(document).ready(function() {
+	createGrid(INITIALSIZE);
+	paintStandard();
+	standard();
+	colorize();
+	chiaroscuro();
 	resetGrid();
 });
 
@@ -10,13 +15,13 @@ function resetGrid() {
 	});
 }
 
-function createGrid() {
-	var size = 10;
+function createGrid(size) {
+	removeGrid();
 
-	var square_size = $('.grid').width()/size;
+	var square_size = $('.grid').width() / size;
 
-	for (var i = 0; i < 10; i++) {
-		for (var j = 0; j < 10; j++) {
+	for (var i = 0; i < size; i++) {
+		for (var j = 0; j < size; j++) {
 			$('<div/>', {
 				class: "square"
 			}).appendTo('.grid');
@@ -25,9 +30,66 @@ function createGrid() {
 
 	$('.square').css('width', square_size);
 	$('.square').css('height', square_size);
+}
 
+function removeGrid() {
+	$('.grid').children('div').remove();
+}
+
+function standard() {	
+ 	$('#standard').on('click', function() {
+		size = input();
+		if (size == 1)
+			return;
+		createGrid(size);
+		paintStandard();
+ 	});
+}
+
+function colorize() {
+	$('#colorize').on('click', function() {
+		size = input();
+		if (size == 1)
+			return;
+		createGrid(size);
+		$('.square').on('mouseenter', function() {
+			var red = Math.floor(Math.random() * 256);
+			var green = Math.floor(Math.random() * 256);
+			var blue = Math.floor(Math.random() * 256);
+			$(this).css({ "background-color": "rgb(" + red + "," + green + "," + blue });
+		});
+	})
+}
+
+function chiaroscuro() {
+	$('#chiaroscuro').on('click', function() {
+		size = input();
+		if (size == 1)
+			return;
+		createGrid(size);
+		$('.square').addClass('painted');
+		$('.square').on('mouseenter', function() {
+			var opacity = $(this).css("opacity")
+			if (opacity != 0) {
+				$(this).css("opacity", opacity - .10);
+			}
+		});
+	});
+}
+
+function paintStandard() {
 	$('.square').on('mouseenter', function() {
 		$(this).addClass('painted');
 	});
+}
 
+function input() {
+	size = prompt("Choose your grid size between 1 - 64");
+
+	if (size < 1 || size > 64) {
+		alert('Sorry, choose a grid between 1 - 64.');
+		return 1;
+	}
+	else
+		return size;
 }
